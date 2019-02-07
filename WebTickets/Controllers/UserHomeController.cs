f332c2bb2736ae60a255062493eec136094b2706
Qi Yanan <43344337+ikeAr@users.noreply.github.com>
2019-02-07 20:00:36 +0800

    Requeest完成

--- WebTickets/Controllers/UserHomeController.cs
+++ WebTickets/Controllers/UserHomeController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebTickets.Repo;
 
 namespace WebTickets.Controllers
 {
     public class UserHomeController : Controller
     {
         // GET: UserHome
-        public ActionResult Index()
+        public ActionResult Index(RequestModel newRequestModel)
         {
-            return View();
+            var handler = new RequestRepo();
+            handler.Add(newRequestModel);
+            return RedirectToAction("Index","Home");
         }
+      
     }
 }
--- WebTickets/Repo/UserRepo.cs
+++ WebTickets/Repo/UserRepo.cs
@@ -1,8 +1,12 @@
 ﻿
 namespace WebTickets.Repo
 {
+    using System;
     using SquirrelFramework.Repository;
     public class UserRepo : RepositoryBase<UserModel>
+    {
+    }
+    public class RequestRepo : RepositoryBase<RequestModel>
     {
     }
 }
