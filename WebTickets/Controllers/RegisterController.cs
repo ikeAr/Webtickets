f45c8e6baecac7c6997aa9f4738a3e91dc39ffae
Qi Yanan <43344337+ikeAr@users.noreply.github.com>
2019-02-08 12:44:07 +0800

    修改

--- WebTickets/Controllers/RegisterController.cs
+++ WebTickets/Controllers/RegisterController.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Web;
 using System.Web.Mvc;
 using WebTickets.Repo;
 
--- WebTickets/Repo/UserRepo.cs
+++ WebTickets/Repo/UserRepo.cs
@@ -1,12 +1,8 @@
-﻿
-namespace WebTickets.Repo
+﻿namespace WebTickets.Repo
 {
-    using System;
     using SquirrelFramework.Repository;
     public class UserRepo : RepositoryBase<UserModel>
     {
     }
-    public class RequestRepo : RepositoryBase<RequestModel>
-    {
-    }
+    
 }
