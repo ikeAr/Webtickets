4275c058186ce232e365b40d820f3e4f7689c0c5
Qi Yanan <43344337+ikeAr@users.noreply.github.com>
2019-02-20 23:02:15 +0800

    。。

---
+++ WebTickets/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebTickets.Models
+{
+    public enum Role
+    {
+        Maintainner,
+        User,
+        Administator,
+    }
+}
