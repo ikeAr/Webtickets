6e1a7cd4da596c4556107d87ad6e3f56baf10026
Qi Yanan <43344337+ikeAr@users.noreply.github.com>
2019-02-20 23:01:22 +0800

    。。

---
+++ WebTickets/Controllers/MaintainerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using WebTickets.Repo;
+
+namespace WebTickets.Controllers
+{
+    public class MaintainerController : Controller
+    {
+        // GET: Maintainer
+        public ActionResult Index()
+        {
+            var requestRepo = new RequestRepo();
+            var requests = requestRepo.GetAll();
+            //ViewBag.
+            return View();
+        }
+    }
+}
---
+++ WebTickets/Controllers/RequestController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using WebTickets.Repo;
+
+namespace WebTickets.Controllers
+{
+    public class RequestController : Controller
+    {
+        RequestRepo requestRepo = new RequestRepo();
+        // GET: UserHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SaveRequest(RequestModel newRequestModel)
+        {
+            this.requestRepo.Add(newRequestModel);
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
