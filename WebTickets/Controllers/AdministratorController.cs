f6587ffb9833509b0d1ee4832304e99df1f0e8bc
Qi Yanan <43344337+ikeAr@users.noreply.github.com>
2019-03-04 23:09:46 +0800

    。。

---
+++ WebTickets/Controllers/AdministratorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebTickets.Models;
+using WebTickets.Repo;
+
+namespace WebTickets.Controllers
+{
+    public class AdministratorController : ControllerBase
+    {
+        // GET: Administrator
+        TeacherRepo teacherRepo = new TeacherRepo();
+        public ActionResult Index(TeacherModel teacher)
+        {
+            teacherRepo.Add(teacher);
+            return View();
+        }
+    }
+}
---
+++ WebTickets/Controllers/TeacherController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTickets.Controllers
+{
+    public class TeacherController : ControllerBase
+    {
+        // GET: Teacher
+       
+        public ActionResult AuditUser()
+        {
+            return View();
+        }
+
+        public ActionResult AuditRequest()
+        {
+            return View();
+        }
+    }
+}
